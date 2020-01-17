512a5004aa657f7496fd25b0fd81948cfb6c83a5
Jason Ma <z5161364@student.unsw.edu.au>
2020-01-17 13:37:38 +1100

    NewItemActivity almost complete! *cough* pushing incomplete code to master *cough*

--- app/src/main/java/com/example/remindmemunni/activitynew/NewItemActivity.kt
+++ app/src/main/java/com/example/remindmemunni/activitynew/NewItemActivity.kt
@@ -1,13 +1,24 @@
 package com.example.remindmemunni.activitynew
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.*
 import com.example.remindmemunni.R
+import java.time.LocalDateTime
 
-class NewItemActivity : AppCompatActivity() {
+class NewItemActivity : AppCompatActivity(), TimePickerDialog.OnTimeSetListener, DatePickerDialog.OnDateSetListener {
+
+    private lateinit var nameEditText: EditText
+    private lateinit var costEditText: EditText
+    private lateinit var costTypeSpinner: Spinner
+    private lateinit var timeEditText: EditText
+
+    private val time = PrimitiveDateTime()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +26,23 @@ class NewItemActivity : AppCompatActivity() {
 
         setSupportActionBar(findViewById(R.id.toolbar_new))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.cost_types_array, R.layout.support_simple_spinner_dropdown_item)
+        costTypeSpinner = findViewById(R.id.cost_type_spinner)
+        costTypeSpinner.adapter = spinnerAdapter
+
+        nameEditText = findViewById(R.id.name_input_field)
+        costEditText = findViewById(R.id.cost_input_field)
+
+        timeEditText = findViewById(R.id.time_input_field)
+        timeEditText.setOnClickListener {
+            val timeDialog = TimePickerFragment()
+            timeDialog.show(supportFragmentManager, "time_dialog")
+            val dateDialog = DatePickerFragment()
+            dateDialog.show(supportFragmentManager, "date_dialog")
+        }
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -38,5 +66,32 @@ class NewItemActivity : AppCompatActivity() {
             Log.d("Nice", "NewActivity unknown button press: ${item?.itemId}")
             super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        time.mHour = hourOfDay
+        time.mMinute = minute
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        time.mYear = year
+        time.mMonth = month
+        time.mDayOfMonth = dayOfMonth
+    }
+
+    private class PrimitiveDateTime(
+        var mYear: Int = 0,
+        var mMonth: Int = 0,
+        var mDayOfMonth: Int = 0,
+        var mHour: Int = 0,
+        var mMinute: Int = 0
+    ) {
+        fun toLocalDateTime(): LocalDateTime? {
+            if (isZero()) {
+                return null
+            }
+            return LocalDateTime.of(mYear, mMonth, mDayOfMonth, mHour, mMinute)
+        }
+        fun isZero(): Boolean = (mYear == 0 && mMonth == 0 && mDayOfMonth == 0 && mHour == 0 && mMinute == 0)
     }
 }
---
+++ app/src/main/java/com/example/remindmemunni/activitynew/TimePickerFragment.kt
@@ -0,0 +1,34 @@
+package com.example.remindmemunni.activitynew
+
+import android.app.Dialog
+import android.app.TimePickerDialog
+import android.os.Bundle
+import android.text.format.DateFormat
+import androidx.fragment.app.DialogFragment
+import java.util.*
+
+class TimePickerFragment : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val c = Calendar.getInstance()
+        var hour = c.get(Calendar.HOUR_OF_DAY)
+        var minute = c.get(Calendar.MINUTE)
+
+        if (arguments?.getBoolean(EXTRA_HAS_TIME) == true) {
+            hour = arguments!!.getInt(EXTRA_HOUR, 0)
+            minute = arguments!!.getInt(EXTRA_MINUTE, 0)
+        }
+
+        return TimePickerDialog(
+            context, activity as TimePickerDialog.OnTimeSetListener,
+            hour, minute,
+            DateFormat.is24HourFormat(activity)
+        )
+    }
+
+    companion object {
+        const val EXTRA_HAS_TIME= "DATED"
+        const val EXTRA_HOUR = "YEAR"
+        const val EXTRA_MINUTE = "MONTH"
+    }
+}
