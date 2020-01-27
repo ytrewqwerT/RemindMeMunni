5e982f8df298cb5c7738dc7b17de9e341e43b185
Jason Ma <z5161364@student.unsw.edu.au>
2020-01-28 10:26:04 +1100

    Finish AlertDialog ScrollySelect thingy (not really, but it'll do for now)

--- app/src/main/java/com/example/remindmemunni/ScrollSpinner.kt
+++ app/src/main/java/com/example/remindmemunni/ScrollSpinner.kt
@@ -1,8 +1,12 @@
 package com.example.remindmemunni
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.util.Log
+import androidx.core.view.marginBottom
+import androidx.core.view.marginTop
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.RecyclerView
@@ -11,6 +15,10 @@ import androidx.recyclerview.widget.SnapHelper
 class ScrollSpinner<T: ListItemViewable>(context: Context, attrs: AttributeSet)
     : RecyclerView(context, attrs) {
 
+    private var mItemViewHeight: Int = 1
+    private val mPaint: Paint = Paint()
+    private var mItems: List<T?> = emptyList()
+    private var mPaddingSize = 1
     private val mAdapter: CustomRecyclerViewAdapter<T> = CustomRecyclerViewAdapter(null)
     private val mSnapHelper: SnapHelper = object : LinearSnapHelper() {
         override fun findTargetSnapPosition(
@@ -25,35 +33,52 @@ class ScrollSpinner<T: ListItemViewable>(context: Context, attrs: AttributeSet)
             }
         }
         private fun boundSize(position: Int): Int = when {
-            position < paddingSize -> paddingSize
-            position > mItems.size - paddingSize -> mItems.size - paddingSize - 1
+            position < mPaddingSize -> mPaddingSize
+            position > mItems.size - mPaddingSize -> mItems.size - mPaddingSize - 1
             else -> position
         }
     }
-    private var mItems: List<T?> = emptyList()
-    private var paddingSize = 5
 
     init {
         super.setAdapter(mAdapter)
         layoutManager = LinearLayoutManager(context)
         mSnapHelper.attachToRecyclerView(this)
+        mPaint.color = resources.getColor(R.color.colorAccent, context.theme)
+        mPaint.strokeWidth = 3F
     }
 
+    override fun onDraw(c: Canvas?) {
+        super.onDraw(c)
+
+        val itemView = mSnapHelper.findSnapView(layoutManager)
+        if (itemView != null) {
+            mItemViewHeight = itemView.height + (itemView.marginTop + itemView.marginBottom) / 2
+        }
+
+        var lineHeight = (top + bottom).toFloat() / 2 - mItemViewHeight.toFloat() / 2
+        c?.drawLine(0F, lineHeight, width.toFloat(), lineHeight, mPaint)
+        lineHeight += mItemViewHeight
+        c?.drawLine(0F, lineHeight, width.toFloat(), lineHeight, mPaint)
+        Log.d("ScrollSpinner", "$this")
+    }
+
+    @Suppress("DeprecatedCallableAddReplaceWith")
+    @Deprecated("Setting adapters is not supported")
     override fun setAdapter(adapter: Adapter<*>?) {
-        throw IllegalAccessException("${ScrollSpinner::class.simpleName} doesn't support setting adapters")
+        throw UnsupportedOperationException(
+            "${ScrollSpinner::class.simpleName} doesn't support setting adapters"
+        )
     }
 
     fun setItems(items: List<T?>) {
         // TODO: Set paddingSize such that there is enough to allow all items to be selected
         val newItems = items.toMutableList()
-        for (i in 1..paddingSize) {
+        for (i in 1..mPaddingSize) {
             newItems.add(0, null)
             newItems.add(null)
         }
-
         mItems = newItems
         mAdapter.setItems(mItems)
-        smoothScrollToPosition(paddingSize + 2)
     }
 
     fun getSelectedItem(): T? {
--- app/src/main/java/com/example/remindmemunni/activitynewseries/RecurrenceSelectFragment.kt
+++ app/src/main/java/com/example/remindmemunni/activitynewseries/RecurrenceSelectFragment.kt
@@ -1,20 +1,18 @@
 package com.example.remindmemunni.activitynewseries
 
 import android.annotation.SuppressLint
-import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 import com.example.remindmemunni.R
 import com.example.remindmemunni.ScrollSpinner
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlin.ClassCastException
 
 class RecurrenceSelectFragment: DialogFragment() {
 
     private lateinit var listener: RecurrenceSelectListener
-
-
 
     interface RecurrenceSelectListener {
         fun onDialogConfirm(dialog: DialogFragment, months: Int, days: Int)
@@ -23,28 +21,25 @@ class RecurrenceSelectFragment: DialogFragment() {
     @SuppressLint("InflateParams")
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
-            val builder = AlertDialog.Builder(it)
             val inflater = requireActivity().layoutInflater
-
             val view = inflater.inflate(R.layout.dialog_frequency, null)
 
             val daysScrollSpinner = view.findViewById<ScrollSpinner<NumberListItem>>(R.id.days_list)
             daysScrollSpinner.setItems(NumberListItem.createSequentialList(0, 30).asReversed())
-
             val monthsScrollSpinner = view.findViewById<ScrollSpinner<NumberListItem>>(R.id.months_list)
             monthsScrollSpinner.setItems(NumberListItem.createSequentialList(0, 24).asReversed())
 
-            builder.setView(view)
-            builder.setPositiveButton("Confirm") { _, _ ->
-                val days = daysScrollSpinner.getSelectedItem()?.num ?: 0
-                val months = monthsScrollSpinner.getSelectedItem()?.num ?: 0
-                listener.onDialogConfirm(this, months, days)
-                dialog?.dismiss()
-            }
-            builder.setNegativeButton("Cancel") { _, _ ->
-                dialog?.cancel()
-            }
-            builder.create()
+            MaterialAlertDialogBuilder(it)
+                .setTitle("Set Frequency")
+                .setView(view)
+                .setPositiveButton("Confirm") { _, _ ->
+                    val days = daysScrollSpinner.getSelectedItem()?.num ?: 0
+                    val months = monthsScrollSpinner.getSelectedItem()?.num ?: 0
+                    listener.onDialogConfirm(this, months, days)
+                    dialog?.dismiss()
+                }.setNegativeButton("Cancel") { _, _ ->
+                    dialog?.cancel()
+                }.create()
         } ?: throw IllegalStateException("Activity cannot be null")
     }
 
