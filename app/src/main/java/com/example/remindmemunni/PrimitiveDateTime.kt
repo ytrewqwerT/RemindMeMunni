353319f07e6fdfb950c5b0d3a50d585e0b46c51c
Jason Ma <z5161364@student.unsw.edu.au>
2020-01-29 11:20:19 +1100

    PrimitiveDateTime Conversions (incl. UnitTest!)

--- app/src/main/java/com/example/remindmemunni/PrimitiveDateTime.kt
+++ app/src/main/java/com/example/remindmemunni/PrimitiveDateTime.kt
@@ -1,16 +1,38 @@
 package com.example.remindmemunni
 
+import java.time.Instant
 import java.time.LocalDateTime
+import java.time.OffsetDateTime
+import java.time.ZoneId
 
-class PrimitiveDateTime {
-    var mYear: Int = 0
-    var mMonth: Int = 0
-    var mDayOfMonth: Int = 0
-    var mHour: Int = 0
+class PrimitiveDateTime(
+    var mYear: Int = 0,
+    var mMonth: Int = 0,
+    var mDayOfMonth: Int = 0,
+    var mHour: Int = 0,
     var mMinute: Int = 0
+) {
 
     fun toLocalDateTime(): LocalDateTime?  = when (mYear) {
         0 -> null
-        else -> LocalDateTime.of(mYear, mMonth+1, mDayOfMonth, mHour, mMinute)
+        else -> LocalDateTime.of(mYear, mMonth, mDayOfMonth, mHour, mMinute)
     }
+
+    fun toEpoch(): Long {
+        val localDateTime = toLocalDateTime()
+        return localDateTime?.atZone(ZoneId.systemDefault())?.toEpochSecond() ?: 0
+    }
+
+     companion object {
+         fun fromLocalDateTime(time: LocalDateTime): PrimitiveDateTime = PrimitiveDateTime(
+             time.year, time.monthValue, time.dayOfMonth,
+             time.hour, time.minute
+         )
+
+         fun fromEpoch(epochSeconds: Long): PrimitiveDateTime {
+             if (epochSeconds == 0L) return PrimitiveDateTime()
+             val localDateTime = LocalDateTime.ofInstant(Instant.ofEpochSecond(epochSeconds), ZoneId.systemDefault())
+             return fromLocalDateTime(localDateTime)
+         }
+     }
 }
--- app/src/test/java/com/example/remindmemunni/ExampleUnitTest.kt
+++ app/src/test/java/com/example/remindmemunni/ExampleUnitTest.kt
@@ -14,4 +14,26 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun primitiveDateTimeConversions() {
+        var pdt = PrimitiveDateTime(2000, 1, 1, 9, 0)
+        var ldt = pdt.toLocalDateTime()
+        assertEquals(2000, ldt?.year)
+        assertEquals(1, ldt?.monthValue)
+        assertEquals(1, ldt?.dayOfMonth)
+        assertEquals(9, ldt?.hour)
+        assertEquals(0, ldt?.minute)
+
+        pdt = PrimitiveDateTime.fromLocalDateTime(ldt!!)
+        assertEquals(pdt.mYear, 2000)
+        assertEquals(pdt.mMonth, 1)
+        assertEquals(pdt.mDayOfMonth, 1)
+        assertEquals(pdt.mHour, 9)
+        assertEquals(pdt.mMinute, 0)
+
+        val testVal = 39600L
+        val epoch = PrimitiveDateTime.fromEpoch(testVal).toEpoch()
+        assertEquals(testVal, epoch)
+    }
 }
