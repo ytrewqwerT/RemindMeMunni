96dfbbded6285054a3950bd1f162e290f2d37105
Jason Ma <ytrewqwert@gmail.com>
2020-09-02 14:36:01 +1000

    ItemFragment: Hide components with no value

--- app/src/main/java/com/example/remindmemunni/destinations/item/ItemFragment.kt
+++ app/src/main/java/com/example/remindmemunni/destinations/item/ItemFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.LiveData
+import com.example.remindmemunni.R
 import com.example.remindmemunni.databinding.FragmentItemBinding
 import com.example.remindmemunni.utils.InjectorUtils
 
@@ -37,11 +39,22 @@ class ItemFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.name.observe(viewLifecycleOwner) { activity?.title = it }
+        hideWhenEmpty(view.findViewById(R.id.series), viewModel.series)
+        hideWhenEmpty(view.findViewById(R.id.category), viewModel.category)
+        hideWhenEmpty(view.findViewById(R.id.cost), viewModel.cost)
+        hideWhenEmpty(view.findViewById(R.id.time), viewModel.time)
+        hideWhenEmpty(view.findViewById(R.id.notify), viewModel.time)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
+    }
+
+    private fun hideWhenEmpty(view: View, source: LiveData<String>) {
+        source.observe(viewLifecycleOwner) {
+            view.visibility = if (it.isEmpty()) View.GONE else View.VISIBLE
+        }
     }
 
     companion object {
--- app/src/main/java/com/example/remindmemunni/destinations/item/ItemViewModel.kt
+++ app/src/main/java/com/example/remindmemunni/destinations/item/ItemViewModel.kt
@@ -9,30 +9,50 @@ import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class ItemViewModel(
-    itemRepository: ItemRepository,
+    private val itemRepository: ItemRepository,
     itemId: Int
 ) : ViewModel() {
 
     private val _name = MutableLiveData("")
     private val _series = MutableLiveData("")
     private val _category = MutableLiveData("")
-    private val _time = MutableLiveData("")
     private val _cost = MutableLiveData("")
+    private val _time = MutableLiveData("")
+    private val _notify = MutableLiveData(false)
     val name: LiveData<String> = _name
     val series: LiveData<String> = _series
     val category: LiveData<String> = _category
-    val time: LiveData<String> = _time
     val cost: LiveData<String> = _cost
+    val time: LiveData<String> = _time
+    val notify: LiveData<Boolean> = _notify
 
     init {
         viewModelScope.launch {
             itemRepository.getItem(itemId).collect {
                 _name.value = it.name
-                if (it.seriesId != 0) _series.value = itemRepository.getDirectSerie(it.seriesId).series.name
-                _category.value = it.category
-                _time.value = it.getDateString()
-                _cost.value = it.getCostString()
+                setSeries(it.seriesId)
+                setCategory(it.category)
+                setTime(it.getDateString())
+                setCost(it.getCostString())
+                _notify.value = it.notify
             }
         }
+    }
+
+    private suspend fun setSeries(seriesId: Int) {
+        _series.value = if (seriesId == 0) ""
+        else "Series: ${itemRepository.getDirectSerie(seriesId).series.name}"
+    }
+    private fun setCategory(categoryString: String) {
+        _category.value = if (categoryString.isEmpty()) ""
+        else "Category: $categoryString"
+    }
+    private fun setTime(timeString: String) {
+        _time.value = if (timeString.isEmpty()) ""
+        else "Time: $timeString"
+    }
+    private fun setCost(costString: String) {
+        _cost.value = if (costString.isEmpty()) ""
+        else "Cost: $costString"
     }
 }
